e31749524e52d08fcf9c38940d109a6f483b30d0
tariq-ventura <tariqventura@gmail,com>
2023-12-03 21:15:25 -0600

    host\

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    socketPath: process.env.DB_PATH
+    host: process.env.DB_PATH
 })
 
 module.exports = connection
