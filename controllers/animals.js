07dcb6cf62c4c3dcffce76d18ecfa94c65e1c92d
tariq-ventura <tariqventura@gmail,com>
2023-12-05 14:02:51 -0600

    connection

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const connection = require('../config/connection')
 
 exports.getAnimals = async (req, res) => {
@@ -31,5 +32,26 @@ exports.insertAnimals = async (req, res) => {
         } else {
             res.send(true)
         }
+    })
+}
+
+exports.testConnection = (req, res) => {
+    var mysql = require('mysql');
+
+    const connection = mysql.createConnection({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_PATH
+    })
+
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            res.send('not connected')
+            return;
+        }
+        res.send('connected')
+        console.log('connected as id ' + connection.threadId);
     })
 }
--- router.js
+++ router.js
@@ -6,13 +6,6 @@ const animals = require('./controllers/animals')
 
 router.get('/api/animales', animals.getAnimals)
 router.post('/api/animales', animals.insertAnimals)
-router.get('/', connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack)
-        return;
-    }
-
-    console.log('connected as id ' + connection.threadId)
-}))
+router.get('/', animals.testConnection)
 
 module.exports = router
