e604c7b211207de848fa61b03e4fd24df3064ca7
tariq-ventura <tariqventura@gmail,com>
2023-12-05 12:49:45 -0600

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
